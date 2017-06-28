ffd503cf9548f3021cf351b8a21a2049e0fa113b
WeiJingPan <466676342@qq.com>
2017-06-29 03:22:14 +0800

    重新梳理策略模式，代理模式

---
+++ 1-StrategyPattern/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_StrategyPattern
+{
+    /// <summary>
+    /// 刘备要到江东娶老婆了，走之前诸葛亮给赵云（伴郎）三个锦囊妙计，说是按天机拆开解决棘手问题，
+    ///嘿，还别说，真是解决了大问题，搞到最后是周瑜陪了夫人又折兵呀，那咱们先看看这个场景是什么样子
+    ///的。
+    ///先说这个场景中的要素：三个妙计，一个锦囊，一个赵云，妙计是小亮同志给的，妙计是放置在锦囊
+    ///里，俗称就是锦囊妙计嘛，那赵云就是一个干活的人，从锦囊中取出妙计，执行，然后获胜.
+    /// 就这三招，搞的周郎是“陪了夫人又折兵”呀！这就是策略模式，高内聚低耦合的特点也表现出来了，
+    /// 还有一个就是扩展性，也就是 OCP 原则，策略类可以继续增加下去，只要修改 Context.java 就可以了，这
+    /// 个不多说了，自己领会吧。
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Context context;
+            Console.WriteLine("----刚刚到吴国的时候拆第一个----------");
+            context=new Context(new BackDoor());
+            context.Operator();
+
+            Console.WriteLine("---------刘备乐不思蜀，拆第二个了----------");
+            context=new Context(new GivenGreenLight());
+            context.Operator();
+
+            Console.WriteLine("--------孙权派出了追兵-----------");
+            context=new Context(new BlockEnemy());
+            context.Operator();
+
+            Console.ReadKey();
+        }
+    }
+
+    public class Context
+    {
+        public IStrategy strategy;
+
+        public Context(IStrategy strategy)
+        {
+            this.strategy = strategy;
+        }
+
+        public void Operator()
+        {
+            strategy.Operator();
+        }
+    }
+
+    public class BlockEnemy : IStrategy
+    {
+        public void Operator()
+        {
+            Console.WriteLine("有追兵，孙夫人垫后");
+        }
+    }
+    public class GivenGreenLight : IStrategy
+    {
+        public void Operator()
+        {
+            Console.WriteLine("给绿灯通行");
+        }
+    }
+    public class BackDoor:IStrategy
+    {
+        public void Operator()
+        {
+            Console.WriteLine("走后门");
+        }
+    }
+    public interface IStrategy
+    {
+        void Operator();
+    }
+}
--- ProxyPattern/Program.cs
+++ ProxyPattern/Program.cs
@@ -6,6 +6,14 @@ using System.Threading.Tasks;
 
 namespace ProxyPattern
 {
+    /// <summary>
+    /// 什么是代理模式呢？我很忙，忙的没空理你，那你要找我呢就先找我的代理人吧，那代理人总要知道
+    /// 被代理人能做哪些事情不能做哪些事情吧，那就是两个人具备同一个接口，代理人虽然不能干活，但是被
+    /// 代理的人能干活呀。
+    /// 说完这个故事，那额总结一下，代理模式主要使用了 面向对象 的多态，干活的是被代理类，代理类主要是
+    /// 接活，你让我干活，好，我交给幕后的类去干，你满意就成，那怎么知道被代理类能不能干呢？同根就成，
+    /// 大家知根知底，你能做啥，我能做啥都清楚的很，同一个接口呗
+    /// </summary>
     class XiMenQing
     {
         static void Main(string[] args)
@@ -18,54 +26,54 @@ namespace ProxyPattern
             wangPo.MakeSomeOneHappyWithMan();
             Console.ReadKey();
         }
-        public class WangPo:KindWomen
+    }
+    public class WangPo : KindWomen
+    {
+        private KindWomen kindWomen;
+        public WangPo()
         {
-            private KindWomen kindWomen;
-            public WangPo()
-            {
-                this.kindWomen = new PanJinLian();
-            }
+            this.kindWomen = new PanJinLian();
+        }
 
-            //她可以是kindWomen的任何一个女人的代理，只要你是这类型的女人
-            public WangPo(KindWomen thisKindWomen)
-            {
-                this.kindWomen = thisKindWomen;
-            }
-            public void MakeSomeOneHappyWithMan()
-            {
-                this.kindWomen.HappyWithMan();//自己老了，干不动这行，可以让年轻的代替
-            }
-            public void MakeSomeOneEyesWithMan()
-            {
-                this.kindWomen.MakeEyeWithMan();//王婆怎么大年纪了，谁还看她抛媚眼？
-            }
-            public void HappyWithMan()
-            {
-                Console.WriteLine("干不动");
-            }
-            public void MakeEyeWithMan()
-            {
-                Console.WriteLine("抛媚眼没人看");
-            }
-        }
-        public class PanJinLian : KindWomen
+        //她可以是kindWomen的任何一个女人的代理，只要你是这类型的女人
+        public WangPo(KindWomen thisKindWomen)
         {
-            public void HappyWithMan()
-            {
-                Console.WriteLine("金莲和男人做的很高兴....");
-            }
-            public void MakeEyeWithMan()
-            {
-                Console.WriteLine("金莲给男人抛媚眼....");
-            }
+            this.kindWomen = thisKindWomen;
         }
-        //此类女人
-        public interface KindWomen
+        public void MakeSomeOneHappyWithMan()
         {
-            //抛媚眼
-            void MakeEyeWithMan();
-            //勾引男人
-            void HappyWithMan();
+            this.kindWomen.HappyWithMan();//自己老了，干不动这行，可以让年轻的代替
         }
+        public void MakeSomeOneEyesWithMan()
+        {
+            this.kindWomen.MakeEyeWithMan();//王婆怎么大年纪了，谁还看她抛媚眼？
+        }
+        public void HappyWithMan()
+        {
+            Console.WriteLine("干不动");
+        }
+        public void MakeEyeWithMan()
+        {
+            Console.WriteLine("抛媚眼没人看");
+        }
+    }
+    public class PanJinLian : KindWomen
+    {
+        public void HappyWithMan()
+        {
+            Console.WriteLine("金莲和男人做的很高兴....");
+        }
+        public void MakeEyeWithMan()
+        {
+            Console.WriteLine("金莲给男人抛媚眼....");
+        }
+    }
+    //此类女人
+    public interface KindWomen
+    {
+        //抛媚眼
+        void MakeEyeWithMan();
+        //勾引男人
+        void HappyWithMan();
     }
 }
