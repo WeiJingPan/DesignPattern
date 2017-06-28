bc5700199f4359a6bece6c764f5f4da5f1931017
WeiJingPan <466676342@qq.com>
2017-06-29 00:04:48 +0800

    中介模式二次更新

--- MediatorPattern/Program.cs
+++ MediatorPattern/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,5 +12,106 @@ namespace MediatorPattern
         static void Main(string[] args)
         {
         }
+    }
+
+    public class Stock : Department
+    {
+        public int mHaveCountOfIBMComputer = 0;
+
+        public Stock(Mediator mediator)
+            :base(mediator)
+        {
+            
+        }
+        public void AddIBMComputer(int number)
+        {
+            mHaveCountOfIBMComputer = mHaveCountOfIBMComputer + number;
+        }
+
+        public void CutIBMComputer(int number)
+        {
+            mHaveCountOfIBMComputer = mHaveCountOfIBMComputer - number;
+        }
+    }
+    public class Sale : Department
+    {
+        public int StatusOfIBMSale = 80;
+        public Sale(Mediator mediator)
+            : base(mediator)
+        {
+            
+        }
+        public void SellIBMComputer(int number)
+        {
+            Console.WriteLine("卖出了"+number+"台IBM电脑");
+        }
+    }
+    public class Purchase:Department
+    {
+        public Purchase(Mediator mediator)
+            : base(mediator)
+        {
+            
+        }
+        public void BuyIBMComputer(int number)
+        {
+            Console.WriteLine("进了"+number+"台IBM电脑");
+        }
+    }
+    public abstract class Department
+    {
+        protected Mediator mMediator;
+
+        public Department(Mediator mediator)
+        {
+            mMediator = mediator;
+        }
+    }
+
+    public class Mediator
+    {
+        private Purchase mPurchase;
+        private Sale mSale;
+        private Stock mStock;
+
+        public Mediator()
+        {
+            mPurchase = new Purchase(this);
+            mSale = new Sale(this);
+            mStock = new Stock(this);
+        }
+
+        public void Handle(PostEvent eEvent,object mObj)
+        {
+            switch (eEvent)
+            {
+                    case PostEvent.eBuyIBMComputer:
+                    BuyIBMComputer((int)mObj);
+                        break;
+                    case PostEvent.eSellIBMComputer:
+                    SellIBMComputer((int)mObj);
+                        break;
+            }
+        }
+
+        private void BuyIBMComputer(int number)
+        {
+            mPurchase.BuyIBMComputer(number);
+            mStock.AddIBMComputer(number);
+        }
+
+        private void SellIBMComputer(int number)
+        {
+            mStock.CutIBMComputer(number);
+            mSale.SellIBMComputer(number);
+        }
+    }
+
+    public enum PostEvent
+    {
+        eBuyIBMComputer,
+        eSellIBMComputer,
+        eOffSell,
+        eClear
     }
 }
